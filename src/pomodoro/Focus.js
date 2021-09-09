11b2086db4da34510defa153b93c1dc35779e14d
SamuelJSherman <86033590+SamuelJSherman@users.noreply.github.com>
2021-09-08 17:30:06 -0700

    update indentation

--- src/pomodoro/Focus.js
+++ src/pomodoro/Focus.js
@@ -1,24 +1,18 @@
 import React from "react";
 
-function Focus({
-    focusDuration,
-    setFocusDuration,
-    isTimerRunning
-    }) {
-
-//focus durations
-return (
-<div className="pomodoro">
-    <div className="row">
+function Focus({ focusDuration, setFocusDuration, isTimerRunning }) {
+  //focus durations
+  return (
+    <div className="pomodoro">
+      <div className="row">
         <div className="col">
-            <div className="input-group input-group-lg mb-2">
-                <span className="input-group-text" data-testid="duration-focus">
-                {/* TODO: Update this text to display the current focus session duration */}
-                Focus Duration: {("0" + focusDuration).substr(-2)}:00
-
-                </span>
-                <div className="input-group-append">
-                {/* TODO: Implement decreasing focus duration and disable during a focus or break session
+          <div className="input-group input-group-lg mb-2">
+            <span className="input-group-text" data-testid="duration-focus">
+              {/* TODO: Update this text to display the current focus session duration */}
+              Focus Duration: {("0" + focusDuration).substr(-2)}:00
+            </span>
+            <div className="input-group-append">
+              {/* TODO: Implement decreasing focus duration and disable during a focus or break session
                 added event listener and name="decrease-focus"*/}
 
               <button
@@ -26,9 +20,9 @@ return (
                 className="btn btn-secondary"
                 data-testid="decrease-focus"
                 onClick={() => {
-                  if(!isTimerRunning && focusDuration > 5)
-                    setFocusDuration(focusDuration -5);
-                  }}
+                  if (!isTimerRunning && focusDuration > 5)
+                    setFocusDuration(focusDuration - 5);
+                }}
               >
                 <span className="oi oi-minus" />
               </button>
@@ -38,18 +32,17 @@ return (
                 className="btn btn-secondary"
                 data-testid="increase-focus"
                 onClick={() => {
-                  if(!isTimerRunning && focusDuration < 60)
-                    setFocusDuration(focusDuration +5);
-                  }}
+                  if (!isTimerRunning && focusDuration < 60)
+                    setFocusDuration(focusDuration + 5);
+                }}
               >
                 <span className="oi oi-plus" />
               </button>
-
-                </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
   );
- }
+}
 export default Focus;
